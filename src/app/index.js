9f9abc95400f81abe74a41724ee8812361caa44b
iznyn <mahtunis@gmail.com>
2021-06-12 21:46:49 +0700

    Prepare redux

--- src/app/index.js
+++ src/app/index.js
@@ -4,19 +4,23 @@
  */
 import React from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import { Http } from 'services/http';
+import store from 'store';
 import Main from './Main';
 
 // Init API service
 Http.init();
 
-const App = () => (
-  <BrowserRouter basename={process.env.APP_BASENAME}>
-    <Switch>
-      <Main />
-    </Switch>
-  </BrowserRouter>
+const App = () => ( 
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.APP_BASENAME}>
+      <Switch>
+        <Main />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
